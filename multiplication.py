9d2e3d901f4c4ba97930c4f509bd3d9d9b38bae5
Siddharth Lokam <sidhu.lokam@gmail.com>
2017-03-31 12:40:57 -0400

    Multiplication
    This multiplies two numbers.

---
+++ multiplication.py
@@ -0,0 +1,7 @@
+def multiplication(number1, number2) :
+    return number1 * number2
+
+number1 = int(input("Enter First Number : "))
+number2 = int(input("Enter Second Number : "))
+number3 = multiplication(number1, number2)
+print("the product of the two numbers is : "+ str(number3))
